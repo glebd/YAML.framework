3ea475326504c39971768417d97337bbcde6f347
Daniel Drzimotta <daniel@somacreates.com>
2012-07-31 14:50:06 -0600

    We now have a mac static libray that we can build.

---
+++ yaml.mac/yaml_mac.h
@@ -0,0 +1,13 @@
+//
+//  yaml_mac.h
+//  yaml.mac
+//
+//  Created by Daniel Drzimotta on 2012-07-31.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface yaml_mac : NSObject
+
+@end
---
+++ yaml.mac/yaml_mac.m
@@ -0,0 +1,13 @@
+//
+//  yaml_mac.m
+//  yaml.mac
+//
+//  Created by Daniel Drzimotta on 2012-07-31.
+//
+//
+
+#import "yaml_mac.h"
+
+@implementation yaml_mac
+
+@end
